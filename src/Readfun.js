79a012e4e00fc6a658ad6f3304816e55dc66f5b7
Siddu Reddy <siddureddy@Siddu-Reddys-MacBook-Air.local>
2024-01-03 15:24:14 +0530

    test

---
+++ src/Readfun.js
@@ -0,0 +1,13 @@
+let read = ()=>{
+    let name="hank"
+    let id=101
+    let email="kjshsl@gmail.com"
+
+    return<div>
+        <h3> Reading variables from function</h3>
+        <h3>empname:{name}</h3>
+        <h3>empid:{id}</h3>
+        <h3>Email:{email}</h3>
+    </div>
+}
+export default read
